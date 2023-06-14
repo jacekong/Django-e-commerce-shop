ebe5e46d73be0f1a79befb8566c6ec8ae37adb64
JaceKong <78537442+jacekong@users.noreply.github.com>
2023-06-14 19:06:53 +0800

    Update settings.py

--- tradingshop/settings.py
+++ tradingshop/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
