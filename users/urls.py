601dd42cec9886f011b1f1023bc6a69bfe87be59
JaceKong <78537442+jacekong@users.noreply.github.com>
2023-06-19 17:54:52 +0800

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin_login', views.admin_login, name='adminlogin'),
-    path('logout_admin', views.logout_admin, name="logoutadmin")
+    path('admin-login', views.admin_login, name='adminlogin'),
+    path('logout-admin', views.logout_admin, name="logoutadmin")
 ]
